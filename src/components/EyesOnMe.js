03fb08f7316c9422f710cb87b1509abae5d891e8
osama akb <osama98akb@gmail.com>
2020-03-22 12:21:14 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -3,7 +3,7 @@ import React from "react";
 class EyesOnMe extends React.Component {
   render() {
     return (
-        <button onb>fire</button>
+        <button onblue={console.log('look here');}>fire</button>
     );
   }
 }
